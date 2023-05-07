ba3ba5455a00a3dc0739c56c11931fd0b706908a
rohit-ash-choudhary <ashhh.choudhary@gmail.com>
2023-05-08 01:57:06 +0530

    jdbc working

---
+++ src/ConnectDB.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+/* connect the database
+ https://www.geeksforgeeks.org/introduction-to-jdbc/
+*/
+ */
+public class ConnectDB {
+    static String connection = null;
+    static String databasename="";
+    static String url="jdbc:mysql://localhost:3306/"+databasename;
+    static String username="root";
+    static String password="Rooh@#2001";
+    public static void main(String[] args) {
+
+
+
+    }
+}
--- src/WhyJDBC.java
+++ src/WhyJDBC.java
@@ -16,6 +16,43 @@ We can use JDBC API to handle database using Java program and can perform the fo
                              It uses some database-specific drivers to effectively connect enterprise applications to databases.
             4.JDBC drivers: To communicate with a data source through JDBC,
                              you need a JDBC driver that intelligently communicates with the respective data source.
+JDBC Drivers
+JDBC drivers are client-side adapters (installed on the client machine, not on the server) that convert requests from Java programs to a protocol that the DBMS can understand. There are 4 types of JDBC drivers:
+
+Type-1 driver or JDBC-ODBC bridge driver
+Type-2 driver or Native-API driver
+Type-3 driver or Network Protocol driver
+Type-4 driver or Thin driver
+
+Types of JDBC Architecture(2-tier and 3-tier)
+The JDBC architecture consists of two-tier and three-tier processing models to access a database. They are as described below:
+
+Two-tier model: A java application communicates directly to the data source. The JDBC driver enables the communication between the application and the data source. When a user sends a query to the data source, the answers for those queries are sent back to the user in the form of results.
+The data source can be located on a different machine on a network to which a user is connected. This is known as a client/server configuration, where the user’s machine acts as a client, and the machine has the data source running acts as the server.
+
+Three-tier model: In this, the user’s queries are sent to middle-tier services, from which the commands are again sent to the data source. The results are sent back to the middle tier, and from there to the user.
+This type of model is found very useful by management information system directors.
+Interfaces of JDBC API
+
+A list of popular interfaces of JDBC API is given below:
+
+Driver interface
+Connection interface
+Statement interface
+PreparedStatement interface
+CallableStatement interface
+ResultSet interface
+ResultSetMetaData interface
+DatabaseMetaData interface
+RowSet interface
+Classes of JDBC API
+A list of popular classes of JDBC API is given below:
+
+DriverManager class
+Blob class
+Clob class
+Types class
+
 */
 
 public class WhyJDBC {
